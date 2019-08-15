a6a510b7c2acbd9ae7e522e3434d20907e2997e3
linh-pham-nz <48110814+linh-pham-nz@users.noreply.github.com>
2019-08-16 10:15:05 +1200

    create route for checkout page

--- routes.js
+++ routes.js
@@ -12,20 +12,15 @@ router.get('/', (req, res) => {
 
 // ROUTE TO CHECKOUT PAGE
 router.get('/checkout', (req, res) => {
-    const checkoutItems = // items in checkout area
-
-    data.groceries.price
-    res.render('checkout/checkoutPage', _______)
+    res.render('checkout/checkoutPage', checkoutItems)
 })
 
 // ROUTE TO EACH INDIVIDUAL ITEM PAGE. 
 // Front-end, please do your html + css in the views/items/itemDescription.hbs file we created.
 router.get('/description/:id', (req, res) => {
     const id = req.params.id
-    // const grabId = data.groceries[id -1]
-
     const groceryItem = data.groceries.find(groceries => groceries.id == req.params.id) 
-    console.log(groceryItem)
+    
     res.render('items/itemDescription', groceryItem)
 })
 
