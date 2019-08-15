1b175d1686ebbb202f113dd5a15e5a8a0fda13e8
linh-pham-nz <48110814+linh-pham-nz@users.noreply.github.com>
2019-08-16 11:06:30 +1200

    start updateCartTotal function

--- checkout.js
+++ checkout.js
@@ -6,9 +6,23 @@ for (var i = 0; i < removeCartItemButtons.length; i++) {
     button.addEventListener('click', function(event) {
         var buttonClicked = event.target
         buttonClicked.parentElement.parentElement.remove()
+        updateCartTotal()
     })
 }
 
+// UPDATE CART TOTAL - need class names
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]     
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        
+        
+    }
+
+}
 
 
 
