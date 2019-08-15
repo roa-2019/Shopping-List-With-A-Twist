41b77c0c3bdf88a628b8481e85cad77301c0cd17
Student <student@pop-os.localdomain>
2019-08-15 16:23:23 +1200

    router test worked

--- routes.js
+++ routes.js
@@ -1,3 +1,11 @@
 const express = require('express')
 const router = express.Router() 
 const data = require('./data.json')   
+
+module.exports = router
+
+
+
+router.get('/', (req, res) => {
+    res.send('Hello world')
+})
