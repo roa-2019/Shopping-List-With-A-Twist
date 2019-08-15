2acba4dc1e035aa45b66c3495f881da0f9c9dfdf
linh-pham-nz <48110814+linh-pham-nz@users.noreply.github.com>
2019-08-16 10:04:58 +1200

    fix route for each individual item

--- routes.js
+++ routes.js
@@ -5,17 +5,28 @@ const data = require('./data.json')
 module.exports = router
 
 
-// ROUTE TO MAIN PAGE
+// ROUTE TO MAIN PAGE DISPLAYING ALL ITEMS
 router.get('/', (req, res) => {
-    res.render('groceryItems/store',)
+    res.render('groceryItems/store', data)
+})
+
+// ROUTE TO CHECKOUT PAGE
+router.get('/checkout', (req, res) => {
+    const checkoutItems = // items in checkout area
+
+    data.groceries.price
+    res.render('checkout/checkoutPage', _______)
 })
 
 // ROUTE TO EACH INDIVIDUAL ITEM PAGE. 
 // Front-end, please do your html + css in the views/items/itemDescription.hbs file we created.
 router.get('/description/:id', (req, res) => {
     const id = req.params.id
-    const grabId = data.groceries[id -1]
-    res.render('items/itemDescription', grabId)
+    // const grabId = data.groceries[id -1]
+
+    const groceryItem = data.groceries.find(groceries => groceries.id == req.params.id) 
+    console.log(groceryItem)
+    res.render('items/itemDescription', groceryItem)
 })
 
 
