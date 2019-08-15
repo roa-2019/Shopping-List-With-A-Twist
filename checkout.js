1c1f224e43183bb7aab94bddcabc16d8468feac7
linh-pham-nz <48110814+linh-pham-nz@users.noreply.github.com>
2019-08-16 10:39:19 +1200

    start remove item button

--- checkout.js
+++ checkout.js
@@ -1,6 +1,19 @@
+var removeCartItemButtons = document.getElementsByClassName('remove')
+
+for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function() {
+        
+    })
+}
+
+
+var addCartItemButtons = document.getElementsByClassName('add')
+
 function totalPrice () {
     const itemPrices = []
     // when add is clicked on item, it will push the price into itemPrices array
     // need eventlistener
-    
+
 }
+
