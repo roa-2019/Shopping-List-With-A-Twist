af336a1c7d50b5c1304f4c6462bda42a823d1e83
linh-pham-nz <48110814+linh-pham-nz@users.noreply.github.com>
2019-08-16 11:20:47 +1200

    clean up of server.js file

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const server = express()
 const router = require('./routes')
 
 server.engine('hbs', hbs({
-    defaultLayout: 'main',
-    extname: 'hbs'
-  }))
-  server.set('view engine', 'hbs')
-  server.use(express.static('public'))
-  server.use(express.urlencoded({extended: false}))
-  server.use(router)
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(express.urlencoded({extended: false}))
+server.use(router)
   
-  module.exports = server 
+module.exports = server 
