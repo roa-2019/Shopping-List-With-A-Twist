3e868fb161d64d1f6dbb3ca5909f370906baabae
linh-pham-nz <48110814+linh-pham-nz@users.noreply.github.com>
2019-08-16 09:43:06 +1200

    created router to access individual item page

--- routes.js
+++ routes.js
@@ -5,7 +5,20 @@ const data = require('./data.json')
 module.exports = router
 
 
-
+// ROUTE TO MAIN PAGE
 router.get('/', (req, res) => {
     res.render('groceryItems/store',)
 })
+
+// ROUTE TO EACH INDIVIDUAL ITEM PAGE. 
+// Front-end, please do your html + css in the views/items/itemDescription.hbs file we created.
+router.get('/description/:id', (req, res) => {
+    const id = req.params.id
+    const grabId = data.groceries[id -1]
+    res.render('items/itemDescription', grabId)
+})
+
+
+
+
+
